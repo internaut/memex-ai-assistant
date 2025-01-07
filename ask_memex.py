3daa22b02afa7cfcaeb4b85976336b5a8e520746
Markus Konrad <post@mkonrad.net>
2025-01-07 16:24:31 +0100

    WIP: python-gnupg seems not to work with multiproc

--- ask_memex.py
+++ ask_memex.py
@@ -1,5 +1,8 @@
 import logging
+import os
 import sys
+import multiprocessing as mp
+from functools import partial
 from io import BytesIO
 from pathlib import Path
 from argparse import ArgumentParser
@@ -14,6 +17,7 @@ DEFAULT_ENCODING = 'utf-8'
 DEFAULT_MEMEXDB_PATH = Path(__file__).parent.resolve() / 'memexdb'
 DEFAULT_EMBEDDINGS_MODEL = "cross-encoder/ms-marco-MiniLM-L-6-v2"
 DEFAULT_NUM_CONTEXT_DOCS = 10
+DEFAULT_N_WORKERS = mp.cpu_count() - 1
 
 
 def printerr(*args):
@@ -22,7 +26,10 @@ def printerr(*args):
 
 
 def decrypt_memex(fpath, encoding):
+    gpg = GPG()
     date = fpath.name.removesuffix('.tar.gpg')
+    print(f'process {os.getpid()} is memex for date {date}')
+
     with open(fpath, 'rb') as f:
         decrypted = gpg.decrypt_file(f)
         if decrypted.ok:
@@ -35,54 +42,54 @@ def decrypt_memex(fpath, encoding):
         else:
             raise SystemError(f'could not decrypt file "{fpath}"; details: {decrypted.status}')
 
-argparser = ArgumentParser()
-argparser.add_argument('query', nargs='?', default=None)
-argparser.add_argument('--memexdb', default=str(DEFAULT_MEMEXDB_PATH))
-argparser.add_argument('--num_context_docs', type=int, default=DEFAULT_NUM_CONTEXT_DOCS)
-argparser.add_argument('--encoding', default=DEFAULT_ENCODING)
-argparser.add_argument('--verbose', action='store_true')
 
-args = argparser.parse_args()
+if __name__ == '__main__':
+    argparser = ArgumentParser()
+    argparser.add_argument('query', nargs='?', default=None)
+    argparser.add_argument('--memexdb', default=str(DEFAULT_MEMEXDB_PATH))
+    argparser.add_argument('--num-context-docs', type=int, default=DEFAULT_NUM_CONTEXT_DOCS)
+    argparser.add_argument('--num-workers', type=int, default=DEFAULT_N_WORKERS)
+    argparser.add_argument('--encoding', default=DEFAULT_ENCODING)
+    argparser.add_argument('--verbose', action='store_true')
 
-logging.basicConfig()
-logger = getLogger(__name__)
-if args.verbose:
-    logger.setLevel(logging.DEBUG)
+    args = argparser.parse_args()
 
-if args.query:
-    q = args.query
-else:
-    q = input('Please enter your query: ')
+    logging.basicConfig()
+    logger = getLogger(__name__)
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
 
-q = q.strip()
+    if args.query:
+        q = args.query
+    else:
+        q = input('Please enter your query: ')
 
-logger.info(f'user query: {q}')
+    q = q.strip()
 
-gpg = GPG()
+    logger.info(f'user query: {q}')
 
-logger.info(f'loading embeddings model "{DEFAULT_EMBEDDINGS_MODEL}"')
-model = CrossEncoder(DEFAULT_EMBEDDINGS_MODEL)
+    logger.info(f'loading embeddings model "{DEFAULT_EMBEDDINGS_MODEL}"')
+    model = CrossEncoder(DEFAULT_EMBEDDINGS_MODEL)
 
-memexdb_path = Path(args.memexdb)
-logger.info(f'loading documents from memexdb at "{memexdb_path}"')
-if not memexdb_path.exists():
-    printerr(f'memexdb path does not exist: "{memexdb_path}"')
-    exit(1)
+    memexdb_path = Path(args.memexdb)
+    logger.info(f'loading and decrypting documents from memexdb at "{memexdb_path}" using {args.num_workers} workers')
+    if not memexdb_path.exists():
+        printerr(f'memexdb path does not exist: "{memexdb_path}"')
+        exit(1)
 
-docs = []
-doc_dates = []
-for i, fpath in enumerate(sorted(memexdb_path.glob('*/*.tar.gpg'))):
-    date, text = decrypt_memex(fpath, encoding=args.encoding)
-    docs.append(text)
-    doc_dates.append(date)
-    if i >= 10:
-        break
+    memexfiles = sorted(memexdb_path.glob('*/*.tar.gpg'))[:40]
+    logger.info(f'found {len(memexfiles)} documents')
+    decrypt_memex_w_enc = partial(decrypt_memex, encoding=args.encoding)
 
-logger.info(f'ranking documents')
-ranks = model.rank(q, docs, top_k=args.num_context_docs)
+    with mp.Pool(args.num_workers) as proc_pool:
+        res = proc_pool.map(decrypt_memex_w_enc, memexfiles, len(memexfiles) // args.num_workers + 1)
+        doc_dates, docs = zip(*res)
 
-for rank in ranks:
-    index = rank['corpus_id']
-    print(f"{rank['score']:.2f}\t{doc_dates[index]}\t{docs[index]}")
+    logger.info(f'ranking documents')
+    ranks = model.rank(q, docs, top_k=args.num_context_docs, num_workers=args.num_workers)
 
-logger.info('done')
+    for rank in ranks:
+        index = rank['corpus_id']
+        print(f"{rank['score']:.2f}\t{doc_dates[index]}\t{docs[index]}")
+
+    logger.info('done')
