2bcbda50e503e685e8b370a9193a08c094963439
Markus Konrad <post@mkonrad.net>
2025-01-10 08:37:29 +0100

    WIP

--- ask_memex.py
+++ ask_memex.py
@@ -1,6 +1,10 @@
 import logging
 import sys
 import multiprocessing as mp
+import string
+import pickle
+import hashlib
+import tempfile
 from functools import partial
 from getpass import getpass
 from io import BytesIO
@@ -11,14 +15,33 @@ from logging import getLogger
 
 import pgpy
 from sentence_transformers import CrossEncoder
+import transformers
+import torch
 
 
+HERE = Path(__file__).parent.resolve()
 DEFAULT_ENCODING = 'utf-8'
-DEFAULT_MEMEXDB_PATH = Path(__file__).parent.resolve() / 'memexdb'
+DEFAULT_MEMEXDB_PATH = HERE / 'memexdb'
 DEFAULT_GPG_PRIV_KEY = Path().home() / '.gnupg' / 'default-sec.asc'
 DEFAULT_EMBEDDINGS_MODEL = "cross-encoder/ms-marco-MiniLM-L-6-v2"
 DEFAULT_NUM_CONTEXT_DOCS = 20
 DEFAULT_N_WORKERS = mp.cpu_count() - 1
+LLM_MODEL_PATH = (HERE / '..' / 'llm_models' / 'Llama-3.2-3B-Instruct').resolve()
+
+CONTEXT_DOC_TEMPLATE = """$date
+----------
+
+$text
+
+///
+"""
+
+INSTRUCTIONS_TEMPLATE = """Die folgenden Texte sind Tagebucheinträge. Sie sind nicht sortiert und starten mit einem 
+Datum im Jahr-Monat-Tag Format, gefolgt von "----------" und im Anschluss dem Texteintrag. Jeder Tagebucheintrag endet
+mit "///". Beantworte alle Fragen in Bezug auf die Tagebucheinträge. Hier sind die Tagebucheinträge:
+
+$documents
+"""
 
 
 def printerr(*args):
@@ -56,6 +79,7 @@ if __name__ == '__main__':
     argparser.add_argument('--num-workers', type=int, default=DEFAULT_N_WORKERS)
     argparser.add_argument('--encoding', default=DEFAULT_ENCODING)
     argparser.add_argument('--verbose', action='store_true')
+    argparser.add_argument('--use-cache', action='store_true')
 
     args = argparser.parse_args()
 
@@ -93,25 +117,68 @@ if __name__ == '__main__':
     logger.info(f'loading embeddings model "{DEFAULT_EMBEDDINGS_MODEL}"')
     model = CrossEncoder(DEFAULT_EMBEDDINGS_MODEL)
 
-    memexdb_path = Path(args.memexdb)
-    logger.info(f'loading and decrypting documents from memexdb at "{memexdb_path}" using {args.num_workers} workers')
-    if not memexdb_path.exists():
-        printerr(f'memexdb path does not exist: "{memexdb_path}"')
-        exit(1)
+    if args.use_cache:
+        h = hashlib.new('sha256')
+        h.update(q.encode())
+        q_hash = h.hexdigest()
+        cachefile = Path(tempfile.gettempdir()) / f'{q_hash}.pickle'
+    else:
+        cachefile = None
 
-    memexfiles = sorted(memexdb_path.glob('*/*.tar.gpg'))
-    logger.info(f'found {len(memexfiles)} documents')
-    decrypt_memex_w_args = partial(decrypt_memex, encoding=args.encoding, key_fpath=args.key, key_passwd=key_passwd)
+    if cachefile and cachefile.exists():
+        logger.info(f'loading documents from cache')
+        with open(cachefile, 'rb') as f:
+            doc_dates, docs = pickle.load(f)
+    else:
+        memexdb_path = Path(args.memexdb)
+        logger.info(f'loading and decrypting documents from memexdb at "{memexdb_path}" using '
+                    f'{args.num_workers} workers')
+        if not memexdb_path.exists():
+            printerr(f'memexdb path does not exist: "{memexdb_path}"')
+            exit(1)
 
-    with mp.Pool(args.num_workers) as proc_pool:
-        res = proc_pool.map(decrypt_memex_w_args, memexfiles, len(memexfiles) // args.num_workers + 1)
-        doc_dates, docs = zip(*res)
+        memexfiles = sorted(memexdb_path.glob('*/*.tar.gpg'))
+        logger.info(f'found {len(memexfiles)} documents')
+        decrypt_memex_w_args = partial(decrypt_memex, encoding=args.encoding, key_fpath=args.key, key_passwd=key_passwd)
+
+        with mp.Pool(args.num_workers) as proc_pool:
+            res = proc_pool.map(decrypt_memex_w_args, memexfiles, len(memexfiles) // args.num_workers + 1)
+            doc_dates, docs = zip(*res)
+
+        if cachefile:
+            logger.info(f'saving documents to cache')
+            with open(cachefile, 'wb') as f:
+                pickle.dump((doc_dates, docs), f)
 
     logger.info(f'ranking documents')
     ranks = model.rank(q, docs, top_k=args.num_context_docs, num_workers=args.num_workers)
 
+    context_documents = []
+    context_templ = string.Template(CONTEXT_DOC_TEMPLATE)
     for rank in ranks:
         index = rank['corpus_id']
-        print(f"{rank['score']:.2f}\t{doc_dates[index]}\t{docs[index]}")
+        context_documents.append(context_templ.substitute(date=doc_dates[index], text=docs[index]))
+
+    instructions_templ = string.Template(INSTRUCTIONS_TEMPLATE)
+    instructions = instructions_templ.substitute(documents='\n'.join(context_documents))
+    print(instructions)
+
+    # pipeline = transformers.pipeline(
+    #     "text-generation",
+    #     model=str(LLM_MODEL_PATH),
+    #     model_kwargs={"torch_dtype": torch.bfloat16},
+    #     device_map="auto",
+    # )
+    #
+    # messages = [
+    #     {"role": "system", "content": instructions},
+    #     {"role": "user", "content": q},
+    # ]
+    #
+    # outputs = pipeline(
+    #     messages,
+    #     max_new_tokens=256,
+    # )
+    # print(outputs[0]["generated_text"][-1])
 
     logger.info('done')
