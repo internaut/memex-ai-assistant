0a50c44a66ebadb9f1e20a7955c13029fc8be5c6
Markus Konrad <post@mkonrad.net>
2025-01-10 09:42:10 +0100

    WIP

--- ask_memex.py
+++ ask_memex.py
@@ -1,4 +1,5 @@
 import logging
+import random
 import sys
 import multiprocessing as mp
 import string
@@ -49,7 +50,50 @@ def printerr(*args):
     print(*args, file=sys.stderr)
 
 
-def decrypt_memex(fpath, encoding, key_fpath, key_passwd):
+def load_documents(memexdb, num_workers, encoding, key, key_passwd, sample, use_cache):
+    memexdb_path = Path(memexdb)
+    if not memexdb_path.exists():
+        printerr(f'memexdb path does not exist: "{memexdb_path}"')
+        exit(1)
+
+    memexfiles = sorted(memexdb_path.glob('*/*.tar.gpg'))
+    logger.info(f'found {len(memexfiles)} documents')
+
+    if use_cache:
+        h = hashlib.new('sha256')
+        h.update(''.join(map(str, memexfiles)).encode() + str(sample).encode())
+        cachefile = Path(tempfile.gettempdir()) / f'{h.hexdigest()}.pickle'
+    else:
+        cachefile = None
+
+    if cachefile and cachefile.exists():
+        logger.info(f'loading documents from cache')
+        with open(cachefile, 'rb') as f:
+            doc_dates, docs = pickle.load(f)
+    else:
+        if sample:
+            sample = min(sample, len(memexfiles))
+            logger.info(f'will sample {sample} documents')
+            memexfiles = random.sample(memexfiles, sample)
+
+        logger.info(f'loading and decrypting {len(memexfiles)} documents from memexdb at "{memexdb_path}" using '
+                    f'{num_workers} workers')
+
+        decrypt_memex_w_args = partial(decrypt_doc, encoding=encoding, key_fpath=key, key_passwd=key_passwd)
+
+        with mp.Pool(num_workers) as proc_pool:
+            res = proc_pool.map(decrypt_memex_w_args, memexfiles)
+            doc_dates, docs = zip(*res)
+
+        if cachefile:
+            logger.info(f'saving documents to cache')
+            with open(cachefile, 'wb') as f:
+                pickle.dump((doc_dates, docs), f)
+
+    return doc_dates, docs
+
+
+def decrypt_doc(fpath, encoding, key_fpath, key_passwd):
     date = fpath.name.removesuffix('.tar.gpg')
 
     msg = pgpy.PGPMessage.from_file(fpath)
@@ -69,6 +113,35 @@ def decrypt_memex(fpath, encoding, key_fpath, key_passwd):
     return date, txtbuffer.read().decode(encoding)
 
 
+def process_input(initial_query):
+    if initial_query:
+        q = initial_query
+    else:
+        q = input('Please enter your query. Enter "quit" to stop the program.\n\n')
+
+    q = q.strip()
+    logger.info(f'user query: {q}')
+
+    if q.lower() == 'quit':
+        return False
+
+    print(f'Finding the {args.num_context_docs} most relevant documents out of {len(docs)} documents ...')
+    logger.info(f'ranking {len(docs)} documents using {args.num_workers} workers')
+    ranks = model.rank(q, docs, top_k=args.num_context_docs, num_workers=args.num_workers, show_progress_bar=True)
+
+    context_documents = []
+    context_templ = string.Template(CONTEXT_DOC_TEMPLATE)
+    for rank in ranks:
+        index = rank['corpus_id']
+        context_documents.append(context_templ.substitute(date=doc_dates[index], text=docs[index]))
+
+    instructions_templ = string.Template(INSTRUCTIONS_TEMPLATE)
+    instructions = instructions_templ.substitute(documents='\n'.join(context_documents))
+    print(instructions)
+
+    return True
+
+
 if __name__ == '__main__':
     argparser = ArgumentParser()
     argparser.add_argument('query', nargs='?', default=None)
@@ -77,24 +150,19 @@ if __name__ == '__main__':
     argparser.add_argument('--memexdb', default=str(DEFAULT_MEMEXDB_PATH))
     argparser.add_argument('--num-context-docs', type=int, default=DEFAULT_NUM_CONTEXT_DOCS)
     argparser.add_argument('--num-workers', type=int, default=DEFAULT_N_WORKERS)
+    argparser.add_argument('--sample', type=int, default=0)
     argparser.add_argument('--encoding', default=DEFAULT_ENCODING)
     argparser.add_argument('--verbose', action='store_true')
     argparser.add_argument('--use-cache', action='store_true')
 
     args = argparser.parse_args()
 
+    print(f'Starting up the assistent ...')
+
     logging.basicConfig()
     logger = getLogger(__name__)
     if args.verbose:
         logger.setLevel(logging.DEBUG)
-
-    if args.query:
-        q = args.query
-    else:
-        q = input('Please enter your query: ')
-
-    q = q.strip()
-    logger.info(f'user query: {q}')
 
     logger.info(f'using GPG key "{args.key}"')
     key, _ = pgpy.PGPKey.from_file(args.key)
@@ -117,68 +185,21 @@ if __name__ == '__main__':
     logger.info(f'loading embeddings model "{DEFAULT_EMBEDDINGS_MODEL}"')
     model = CrossEncoder(DEFAULT_EMBEDDINGS_MODEL)
 
-    if args.use_cache:
-        h = hashlib.new('sha256')
-        h.update(q.encode())
-        q_hash = h.hexdigest()
-        cachefile = Path(tempfile.gettempdir()) / f'{q_hash}.pickle'
-    else:
-        cachefile = None
+    print('Loading memex documents ...')
+    doc_dates, docs = load_documents(
+        memexdb=args.memexdb,
+        num_workers=args.num_workers,
+        encoding=args.encoding,
+        key=args.key,
+        key_passwd=key_passwd,
+        sample=args.sample,
+        use_cache=args.use_cache
+    )
 
-    if cachefile and cachefile.exists():
-        logger.info(f'loading documents from cache')
-        with open(cachefile, 'rb') as f:
-            doc_dates, docs = pickle.load(f)
-    else:
-        memexdb_path = Path(args.memexdb)
-        logger.info(f'loading and decrypting documents from memexdb at "{memexdb_path}" using '
-                    f'{args.num_workers} workers')
-        if not memexdb_path.exists():
-            printerr(f'memexdb path does not exist: "{memexdb_path}"')
-            exit(1)
+    cont = True
+    initial_query = args.query
+    while cont:
+        cont = process_input(initial_query)
+        initial_query = None
 
-        memexfiles = sorted(memexdb_path.glob('*/*.tar.gpg'))
-        logger.info(f'found {len(memexfiles)} documents')
-        decrypt_memex_w_args = partial(decrypt_memex, encoding=args.encoding, key_fpath=args.key, key_passwd=key_passwd)
-
-        with mp.Pool(args.num_workers) as proc_pool:
-            res = proc_pool.map(decrypt_memex_w_args, memexfiles, len(memexfiles) // args.num_workers + 1)
-            doc_dates, docs = zip(*res)
-
-        if cachefile:
-            logger.info(f'saving documents to cache')
-            with open(cachefile, 'wb') as f:
-                pickle.dump((doc_dates, docs), f)
-
-    logger.info(f'ranking documents')
-    ranks = model.rank(q, docs, top_k=args.num_context_docs, num_workers=args.num_workers)
-
-    context_documents = []
-    context_templ = string.Template(CONTEXT_DOC_TEMPLATE)
-    for rank in ranks:
-        index = rank['corpus_id']
-        context_documents.append(context_templ.substitute(date=doc_dates[index], text=docs[index]))
-
-    instructions_templ = string.Template(INSTRUCTIONS_TEMPLATE)
-    instructions = instructions_templ.substitute(documents='\n'.join(context_documents))
-    print(instructions)
-
-    # pipeline = transformers.pipeline(
-    #     "text-generation",
-    #     model=str(LLM_MODEL_PATH),
-    #     model_kwargs={"torch_dtype": torch.bfloat16},
-    #     device_map="auto",
-    # )
-    #
-    # messages = [
-    #     {"role": "system", "content": instructions},
-    #     {"role": "user", "content": q},
-    # ]
-    #
-    # outputs = pipeline(
-    #     messages,
-    #     max_new_tokens=256,
-    # )
-    # print(outputs[0]["generated_text"][-1])
-
-    logger.info('done')
+    print('Done.')
